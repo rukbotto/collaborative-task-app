7f83eb184e4d43ba23a5a7855448da6423f59d71
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2025-04-24 18:47:59 +0000

    Update database configuration to use PostgreSQL and environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -74,8 +75,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "default_db_name"),
+        "USER": os.getenv("POSTGRES_USER", "default_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "default_password"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),  # Default to 'db' as per docker-compose.yml
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),  # Default PostgreSQL port
     }
 }
 
