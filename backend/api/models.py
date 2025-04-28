76ca0c4a9744cdc64a880f5eda02efb24ab17c75
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2025-04-28 00:26:32 +0000

    Add Task model and corresponding migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
